4b4be844ea95e9d84ac710ae24c41f922cd6f613
destripador2000 <davidhumbertop503@gmail.com>
2026-05-08 07:49:37 -0600

    Feat: Creación de interfaces para productos, identicas a los schemas en pydantic

--- frontend/src/domain/product/types.ts
+++ frontend/src/domain/product/types.ts
@@ -1,4 +1,30 @@
+export interface Distributor {
+  id: number;
+  name: string;
+  contact_email: string | null;
+  phone: string | null;
+  address: string | null;
+  is_active: boolean;
+}
+
 export interface Product {
+  id: number;
+  distributor_id: number;
+  name: string;
+  description: string | null;
+  price: number;
+  is_discount: boolean;
+  stock: number;
+  category: string;
+  distributor: Distributor | null;
+}
+
+export interface ProductFrontend extends Product {
+  imageUrl: string;
+  imageAlt: string;
+}
+
+export interface ProductMock {
   id: string;
   title: string;
   price: number;
